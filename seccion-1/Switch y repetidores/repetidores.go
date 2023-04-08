89e17b6f876e51f99c946a6a00327c73eb48ba0d
ludmilaalvarez <ludmi096@gmail.com>
2023-04-08 16:12:34 -0300

    tarea

--- seccion-1/Switch y repetidores/repetidores.go
+++ seccion-1/Switch y repetidores/repetidores.go
@@ -8,15 +8,6 @@ type Planetas struct {
 }
 
 func main() {
-	/* 	mercurio := Planetas{"Mercurio", 0}
-	   	venus := Planetas{"Venus", 0}
-	   	tierra := Planetas{"Tierra", 1}
-	   	marte := Planetas{"Marte", 2}
-	   	jupiter := Planetas{"Jupiter", 62}
-	   	saturno := Planetas{"Saturno", 63}
-	   	urano := Planetas{"Urano", 27}
-	   	neptuno := Planetas{"Neptuno", 13}
-	*/
 
 	nombres := []string{"Mercurio", "Venus", "Tierra", "Marte", "Jupiter", "Saturno", "Urano", "Neptuno"}
 	lunas := []int{0, 0, 1, 2, 62, 63, 27, 13}
